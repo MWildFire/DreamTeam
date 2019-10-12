dfb02f77fe3c1ba9191b96d444e374573f44436c
Pocik <nikitaagafonov228@mail.ru>
2019-10-12 17:06:46 +0300

    Added NoWorking Platforms

--- DreamTeam/Assets/PlayerController.cs
+++ DreamTeam/Assets/PlayerController.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-    public float speed = 156;
-    private float speed1 = 156;
-    public Rigidbody rb;
-    public GameObject Player;
-    public GameObject Camera;
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float moveHor = Input.GetAxis("Horizontal");
-        float moveVer = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(moveHor, 0, moveVer);
-        rb.AddForce(move);
+namespace Game
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float speed = 156;
+        private float speed1 = 156;
+        public Rigidbody rb;
+        public GameObject Player;
+        public GameObject Camera;
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float moveHor = Input.GetAxis("Horizontal");
+            float moveVer = Input.GetAxis("Vertical");
+            Vector3 move = new Vector3(moveHor, 0, moveVer);
+            rb.AddForce(move);
 
 
-	}
+        }
+    }
 }
